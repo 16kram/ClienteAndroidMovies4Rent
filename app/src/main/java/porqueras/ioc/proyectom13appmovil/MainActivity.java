162396bc6f45fedefce3d17113f5157da1b8e7e7
Esteban <16k.ram@gmail.com>
2023-03-07 20:01:28 +0100

    Se añade la pantalla principal

--- app/src/main/java/porqueras/ioc/proyectom13appmovil/MainActivity.java
+++ app/src/main/java/porqueras/ioc/proyectom13appmovil/MainActivity.java
@@ -3,12 +3,26 @@ package porqueras.ioc.proyectom13appmovil;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText usuario;
+    EditText contrasena;
+    Button buttonEntrar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Oculta la ActionBar
+        getSupportActionBar().hide();
+
+        //Añadimos los campos de texto y el botón
+        usuario = (EditText) findViewById(R.id.editTextTextUsuario);
+        contrasena = (EditText) findViewById(R.id.editTextTextContrasena);
+        buttonEntrar = (Button) findViewById(R.id.buttonEntrar);
+
     }
 }
